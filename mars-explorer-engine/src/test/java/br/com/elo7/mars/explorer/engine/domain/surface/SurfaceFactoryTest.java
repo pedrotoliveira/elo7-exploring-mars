6cd88f0d38dbef67f92ba2796c52c52ab817ef55
Pedro T. Oliveira <pedro.oliveira20@gmail.com>
2016-04-30 08:33:19 -0300

    SurfaceFactory Test - Success

--- mars-explorer-engine/src/test/java/br/com/elo7/mars/explorer/engine/domain/surface/SurfaceFactoryTest.java
+++ mars-explorer-engine/src/test/java/br/com/elo7/mars/explorer/engine/domain/surface/SurfaceFactoryTest.java
@@ -1,31 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.elo7.mars.explorer.engine.domain.surface;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-/**
- *
- * @author p-poliveira
- */
-public class SurfaceFactoryTest {
-	
-	public SurfaceFactoryTest() {
-	}
+import br.com.elo7.mars.explorer.engine.domain.Factory;
+import br.com.elo7.mars.explorer.engine.test.FixtureTest;
+import br.com.six2six.fixturefactory.function.impl.RandomFunction;
+import org.junit.Before;
 
-	@Test
-	public void testCreate() {
-		System.out.println("create");
-		String input = "";
-		SurfaceFactory instance = new SurfaceFactory();
-		Surface expResult = null;
-		Surface result = instance.create(input);
-		assertEquals(expResult, result);
-		fail("The test case is a prototype.");
-	}
-	
+/**
+ * Surface Factory Tests
+ *
+ * @author pedrotoliveira
+ */
+public class SurfaceFactoryTest extends FixtureTest {
+
+    private Factory<Surface> factory;
+
+    @Before
+    public void setUp() {
+        factory = new SurfaceFactory();
+    }
+
+    @Test
+    public void testCreate() {
+        String input = validInput();
+        Surface surface = factory.create(input);
+        assertNotNull(surface);
+    }
+
+    private String validInput() {
+        return String.format("%d %d", randomInt(0, 1000), randomInt(0, 1000));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNullInput() {
+        factory.create(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidArgument() {
+        String input = invalidInput();
+        factory.create(input);
+    }
+
+    private String invalidInput() {
+        return new RandomFunction(String.class,
+                new String[]{"2 s 1", "  5   5 ", "2 3 4", "q 1"}).generateValue();
+    }
 }
--- mars-explorer-engine/src/test/java/br/com/elo7/mars/explorer/engine/domain/surface/SurfaceSuite.java
+++ mars-explorer-engine/src/test/java/br/com/elo7/mars/explorer/engine/domain/surface/SurfaceSuite.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.elo7.mars.explorer.engine.domain.surface;
 
 import org.junit.runner.RunWith;
@@ -10,10 +5,9 @@ import org.junit.runners.Suite;
 
 /**
  *
- * @author p-poliveira
+ * @author pedrotoliveira
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({br.com.elo7.mars.explorer.engine.domain.surface.SurfaceFactoryTest.class, br.com.elo7.mars.explorer.engine.domain.surface.ScanResultTest.class})
+@Suite.SuiteClasses({br.com.elo7.mars.explorer.engine.domain.surface.SurfaceFactoryTest.class})
 public class SurfaceSuite {
-	
 }
