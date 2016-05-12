cbcbd1e73ff186217c7a57a0f113f74da9338d77
Pedro T. Oliveira <pedro.oliveira20@gmail.com>
2016-05-12 19:01:02 -0300

    Implementing Exeception Mapper

--- mars-explorer/mars-explorer-api/src/main/java/br/com/elo7/mars/explorer/api/endpoint/UnhandledExceptionMapper.java
+++ mars-explorer/mars-explorer-api/src/main/java/br/com/elo7/mars/explorer/api/endpoint/UnhandledExceptionMapper.java
@@ -1,5 +1,8 @@
 package br.com.elo7.mars.explorer.api.endpoint;
 
+import br.com.elo7.mars.explorer.api.resource.error.Message;
+import javax.validation.ValidationException;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -7,6 +10,14 @@ import javax.ws.rs.ext.Provider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import static br.com.elo7.mars.explorer.api.resource.error.MessageType.Endpoint_Error;
+import static br.com.elo7.mars.explorer.api.resource.error.MessageType.Internal_Architecture_Error;
+import static br.com.elo7.mars.explorer.api.resource.error.MessageType.Parameter_Error;
+import static br.com.elo7.mars.explorer.api.resource.error.MessageType.Unexpected_Error;
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
 /**
  * Map a Unhandled Exception to some error message.
@@ -20,12 +31,48 @@ public class UnhandledExceptionMapper implements ExceptionMapper<Exception> {
 	private static final Logger logger = LoggerFactory.getLogger(UnhandledExceptionMapper.class);
 
 	@Override
-	public Response toResponse(Exception exception) {
-		logger.info("Unexpected Exception", exception);
-		return Response
-				.status(Response.Status.INTERNAL_SERVER_ERROR)
-				.entity("Oops! We Have A Problem.")
-				.type(MediaType.APPLICATION_JSON)
-				.build();
+	public Response toResponse(Exception ex) {		
+		if (ex instanceof IllegalArgumentException
+				|| ex instanceof ValidationException) {
+			return badRequest(ex);
+		} else if (ex instanceof NotFoundException) {
+			return notFound(ex);
+		} else if (ex instanceof RuntimeException) {
+			return internalArchtectureError(ex);
+		} else {
+			return unexpectedError(ex);
+		}
+	}
+
+	private Response badRequest(Exception ex) {
+		Message message = new Message(Parameter_Error, BAD_REQUEST);
+		message.addNotification(ex.getMessage());
+		logError(message, ex);
+		return Response.status(BAD_REQUEST).type(MediaType.APPLICATION_JSON).entity(message).build();
+	}
+
+	private Response notFound(Exception ex) {
+		Message message = new Message(Endpoint_Error, NOT_FOUND);
+		message.addNotification(ex.getMessage());
+		logError(message, ex);
+		return Response.status(NOT_FOUND).type(MediaType.APPLICATION_JSON).entity(message).build();
+	}
+
+	private Response internalArchtectureError(Exception ex) {
+		Message message = new Message(Internal_Architecture_Error, INTERNAL_SERVER_ERROR);
+		message.addNotification(ex.getMessage());
+		logError(message, ex);
+		return Response.status(INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON).entity(message).build();
+	}
+
+	private Response unexpectedError(Exception ex) {
+		Message message = new Message(Unexpected_Error, INTERNAL_SERVER_ERROR);
+		message.addNotification(ex.getMessage());
+		logError(message, ex);
+		return Response.status(INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON).entity(message).build();
+	}
+
+	private void logError(Message message, Exception ex) {
+		logger.error(message.toString(), ex);
 	}
 }
--- mars-explorer/mars-explorer-api/src/test/java/br/com/elo7/mars/explorer/api/endpoint/UnhandledExceptionMapperTest.java
+++ mars-explorer/mars-explorer-api/src/test/java/br/com/elo7/mars/explorer/api/endpoint/UnhandledExceptionMapperTest.java
@@ -1,28 +1,79 @@
 package br.com.elo7.mars.explorer.api.endpoint;
 
+import br.com.elo7.mars.explorer.api.resource.error.Message;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.junit.Before;
 import org.junit.Test;
 
+import static br.com.elo7.mars.explorer.api.resource.error.MessageType.Endpoint_Error;
+import static br.com.elo7.mars.explorer.api.resource.error.MessageType.Internal_Architecture_Error;
+import static br.com.elo7.mars.explorer.api.resource.error.MessageType.Parameter_Error;
+import static br.com.elo7.mars.explorer.api.resource.error.MessageType.Unexpected_Error;
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
 /**
- *
  * @author pedrotoliveira
  */
 public class UnhandledExceptionMapperTest {
 	
-	public UnhandledExceptionMapperTest() {
+	private UnhandledExceptionMapper mapper;
+
+	@Before
+	public void setUp() {
+		this.mapper = new UnhandledExceptionMapper();
 	}
 
 	@Test
-	public void testToResponse() {
-		System.out.println("toResponse");
-		Exception exception = null;
-		UnhandledExceptionMapper instance = new UnhandledExceptionMapper();
-		Response expResult = null;
-		Response result = instance.toResponse(exception);
-		assertEquals(expResult, result);
-		fail("The test case is a prototype.");
+	public void testBadRequest() {
+		Exception ex = new IllegalArgumentException("Validation Error");
+		assertThat(mapper.toResponse(ex).getEntity(), equalTo(badRequest(ex).getEntity()));
 	}
 	
+	private Response badRequest(Exception ex) {
+		Message message = new Message(Parameter_Error, BAD_REQUEST);
+		message.addNotification(ex.getMessage());
+		return Response.status(BAD_REQUEST).type(MediaType.APPLICATION_JSON).entity(message).build();
+	}
+	
+	@Test
+	public void testNotFound() {
+		Exception ex = new NotFoundException("Not Found");
+		assertThat(mapper.toResponse(ex).getEntity(), equalTo(notFound(ex).getEntity()));
+	}
+
+	private Response notFound(Exception ex) {
+		Message message = new Message(Endpoint_Error, NOT_FOUND);
+		message.addNotification(ex.getMessage());
+		return Response.status(NOT_FOUND).type(MediaType.APPLICATION_JSON).entity(message).build();
+	}
+	
+	@Test
+	public void testInternalArchtectureError() {
+		Exception ex = new RuntimeException("Some Framework/Implementation Error");
+		assertThat(mapper.toResponse(ex).getEntity(), equalTo(internalArchtectureError(ex).getEntity()));
+	}
+
+	private Response internalArchtectureError(Exception ex) {
+		Message message = new Message(Internal_Architecture_Error, INTERNAL_SERVER_ERROR);
+		message.addNotification(ex.getMessage());
+		return Response.status(INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON).entity(message).build();
+	}
+	
+	@Test
+	public void testUnexpectedError() {
+		Exception ex = new Exception("Big Exception");
+		assertThat(mapper.toResponse(ex).getEntity(), equalTo(unexpectedError(ex).getEntity()));
+	}
+
+	private Response unexpectedError(Exception ex) {
+		Message message = new Message(Unexpected_Error, INTERNAL_SERVER_ERROR);
+		message.addNotification(ex.getMessage());
+		return Response.status(INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON).entity(message).build();
+	}
 }
